e454eb9120f0ed5d9322b5b0065b38b9a3e0de0f
zzacong <ongzz100@gmail.com>
2020-10-29 17:59:48 +1100

    change require dotenv

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,3 @@
-if (process.env.NODE_ENV === 'Development') require('dotenv').config()
-
 module.exports = {
   MongoUri: process.env.MONGO_URI,
 }
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
